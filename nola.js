5b97a85013d571be5ee08708e36b6104f01242bd
Dimitri Ferguson <dimitri.ferguson93@gmail.com>
2021-08-16 21:58:04 -0400

    added files to be updated

--- nola.js
+++ nola.js
@@ -1,22 +1,46 @@
-<<<<<<< HEAD
 setTimeout(time, 4000);
 
 function time() {
   alert("Welcome to The Nola");
 }
 
-$(document).ready(function () {
-  $("#hide").click(function () {
-    $("p").hide();
-  });
-  $("#show").click(function () {
-    $("p").show();
-  });
-});
-=======
-setTimeout(time, 4000);
+// //fetch(
+// //  "https://api.openweathermap.org/data/2.5/weather?q=new+orleans&units=imperial&appid=2d8f7a8386dde22a3fe8bc2c4e1ca86c"
+// //)
+//   .then((response) => response.json())
+//   .then((data) => console.log(data));
 
-function time() {
-  alert("Welcome to The Nola");
+const api_url =
+  "https://api.openweathermap.org/data/2.5/weather?q=new+orleans&units=imperial&appid=2d8f7a8386dde22a3fe8bc2c4e1ca86c";
+
+async function getapi(url) {
+  const response = await fetch(url);
+
+  let data = await response.json();
+  console.log(data);
+  if (response) {
+    hideloader();
+  }
+  show(data);
 }
->>>>>>> afbc9dbeb565c8e8b9567a2d9ec5444106506764
+
+getapi(api_url);
+
+function hideloader() {
+  document.getElementById("loading").style.display = "none";
+}
+function show(data) {
+  let tab = `<tr>
+        <th>Temp</th>
+        <th>Humidity</th>
+      </tr>`;
+
+  for (let key of Object.keys(data.main)) {
+    tab += `<tr>
+        <td>${key.temp}</td>
+        <td>${key.humidity}</td>
+      </tr>`;
+  }
+
+  document.getElementById("sunshine").innerHTML = tab;
+}
